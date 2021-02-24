e63c4d149787c1d2eff350b7f0a4b10542dbca97
taunivance <Enter your email address>
2021-02-23 22:04:23 -0800

    update

--- lesson7/js/imagehandling.js
+++ lesson7/js/imagehandling.js
@@ -3,19 +3,19 @@ const imagesToLoad = document.querySelectorAll("img[data-src]");
 
 //optional parameters being set for the IntersectionalObserver
 const imgOptions = {
-    threshold: 0,
+    threshold: 1,
     rootMargin: "0px 0px 50px 0px"
 };
 
 const loadImages = (image) => {
-    image.setAttribute("src", image.getAttribute ("data-src"));
+    image.setAttribute('src', image.getAttribute ('data-src'));
     image.onload = () => {
         image.removeAttribute("data-src");
     };
 };
 
 //first check to see if Intersection Observer is supported
-if("IntersectionObserver" in window) {
+if('IntersectionObserver' in window) {
     const imgObserver = new IntersectionObserver((items, observer) => {
         items.forEach((item) => { 
             if (item.isIntersecting) {
