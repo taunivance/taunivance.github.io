8b859b45a1d83156a39933b3b6396f060296802c
taunivance <Enter your email address>
2021-02-27 23:02:29 -0800

    update

---
+++ Lesson8/js/storm.js
@@ -0,0 +1,3 @@
+function changeSeverity(val) {
+    document.getElementById('severity').innerHTML = val; 
+  }
---
+++ Lesson8/js/toggle.js
@@ -0,0 +1,4 @@
+document.getElementById("lastupdated").textContent = "Last Updated: " + document.lastModified;
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
