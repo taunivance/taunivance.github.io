03a010f01b3e37475cfc6581a9a41878b1f2340f
taunivance <Enter your email address>
2021-03-16 20:42:41 -0700

    update

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,10 +1,16 @@
-const requestURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&Imperial&appid=2b46ec7de5911d2fd80baa005669e116';
-
+const apiURL =
+  "https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=2b46ec7de5911d2fd80baa005669e116&units=imperial";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    //console.log(jsObject);
-    const temperature = document.querySelector ('#temperature');
-    temperature.textContent = jsObject.main.temp;
-
+    //console.log(jsObject);//
+    document.getElementById("current-temp").textContent = jsObject.main.temp;
+    const imagesrc =
+      "https://openweathermap.org/img/w/" + jsObject.weather[0].icon + ".png";
+    const desc = jsObject.weather[0].description;
+    document.getElementById("imagesrc").textContent = imagesrc;
+    document.getElementById("icon").setAttribute("src", imagesrc);
+    document.getElementById("icon").setAttribute("alt", desc);
   });
+
+ 
