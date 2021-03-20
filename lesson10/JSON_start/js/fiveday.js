246e2c615a91172af198f2f5e8d4a1baae81bad5
taunivance <Enter your email address>
2021-03-19 21:37:21 -0700

    updates

--- lesson10/JSON_start/js/fiveday.js
+++ lesson10/JSON_start/js/fiveday.js
@@ -1,59 +1,63 @@
-const apiURL = "//api.openweathermap.org/data/2.5/forecast?id=5604473&appid=2b46ec7de5911d2fd80baa005669e116&units=imperial";
-
-fetch(apiURL)
-  .then((response) => response.json())
-  .then((weatherForecast) => {
-    //Once it comes back, display it to the console.
-    console.log(weatherForecast);
-    console.log(weatherForecast.city.name);
-    const mylist = weatherForecast.list
-
-  document.getElementById ('place').innerHTML=weatherForecast.city.name;
-
- 
-  
-  const weekDay = new Array(7);
-  weekDay[0] = "Sunday";
-  weekDay[1] = "Monday";
-  weekDay[2] = "Tuesday";
-  weekDay[3] = "Wednesday";
-  weekDay[4] = "Thursday";
-  weekDay[5] = "Friday";
-  weekDay[6] = "Saturday";
-
   const d = new Date();
 
-  const todayDayNumber = 
-  d.getDay();
+  const todayDayNumber = d.getDay();
 
-  let forecastDayNumber = 
-  todayDayNumber;
+  const weekday = new Array(7);
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
 
-  console.log(forecastDayNumber);
+  const apiURL = "//api.openweathermap.org/data/2.5/forecast?id=5604473&appid=2b46ec7de5911d2fd80baa005669e116&units=imperial";
 
-  for (i=0; i<mylist.length;i++) {
-    var time = mylist[i].dt_txt;
-    if (time.includes('19:00:00')){
-      forecastDayNumber+=1;
-      if (forecastDayNumber===7){
-        forecastDayNumber=0;
-      }
+  fetch(apiURL)
+    .then((response) => response.json())
+    .then((weatherInfo) => {
+      //Once it comes back, display it to the console.
+      console.log(weatherInfo);
 
-   
-        let theDayName = document.createElement ("span");
-        theDayName.textContent=weekDay[forecastDayNumber];
-        console.log(">"+weekDay[forecastDayNumber]);
-      
+      document.getElementById ('place').textContent=weatherInfo.city.name;
 
-        let theTemp = document.createElement ("p");
-        theTemp.textContent = weatherForecast.list[i].main.temp + "\xB0";
-  
+      let mylist = weatherInfo.list;
 
-      weatherForcast(mylist[i], weekDay[forecastDayNumber])
+          let forecastDayNumber = todayDayNumber; 
 
+          for (i=0; i<mylist.length;i++) {
+
+                let time = mylist[i].dt_txt;
+
+                if (time.includes('19:00:00')){
+                  console.log("Found an entry with 19:00:00 in the time. It was report "+i+" from the mylist of 40");
+
+                  forecastDayNumber+=1;
+                  if (forecastDayNumber===7){forecastDayNumber=0;}
+
+          
+                  let theDayName = document.createElement ("span");
+                  theDayName.textContent=weekday[forecastDayNumber];
+                  console.log(">"+weekDay[forecastDayNumber]);           
+
+                  let theTemp = document.createElement ("p");
+                  theTemp.textContent = weatherInfo.list[i].main.temp + "\xB0";
+
+                  let iconcode=weatherInfo.list[i].weather[0].icon;
+                  let iconPath = "//openweathermap.org/img/w/" + iconcode + ".png";
+                  let theIcon = document.createElement("img");
+                  theIcon.src=iconPath;
+
+                  let theDay = document.createElement ("div");
+                  theDay.appendChild (theDayName);
+                  theDay.appendChild (theTemp);
+                  theDay.appendChild (theIcon);
+
+                  document.getElementById('weatherforecast').appendChild(theDay);
+     
+            
     }//end if
   }//end for
 
 }); //end of "then" fat arrow function
 
-https://matthewfelton.github.io/lesson11/preston.html
