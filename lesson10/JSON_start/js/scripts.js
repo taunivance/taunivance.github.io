ac3829ae0ce1a22b213f83f165e9ee13e10fb92b
taunivance <Enter your email address>
2021-03-21 11:55:47 -0700

    update

--- lesson10/JSON_start/js/scripts.js
+++ lesson10/JSON_start/js/scripts.js
@@ -1,5 +1,5 @@
 //ADD the key and change units to imperial
-const apiURL = "//api.openweathermap.org/data/2.5/weather?zip=02111,us&appid=2b46ec7de5911d2fd80baa005669e116&units=imperial"
+const apiURL = "//api.openweathermap.org/data/2.5/weather?zip=02111,us&appid=2b46ec7de5911d2fd80baa005669e116&units=imperial";
 
 //Go fetch it and then wait for a response.
 fetch(apiURL)
--- lesson10/js/fiveday.js
+++ lesson10/js/fiveday.js
@@ -58,4 +58,39 @@ fetch(apiURL)
         } //end if
     } //end for
 
- }); //end of "then" fat arrow function
+  }); //end of "then" fat arrow function
+
+
+          const apiURL2 = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=2b46ec7de5911d2fd80baa005669e116&units=imperial";
+          fetch(apiURL2)
+          .then((response) => response.json())
+          .then((weatherinfo) => {
+            //Once it comes back, display it to the console.
+            console.log(weatherinfo);
+            
+            document.getElementById('place').innerHTML=weatherinfo.name;
+            let current = `${weatherinfo.weather[0].description}, ${weatherinfo.main.temp}`;
+            document.getElementById("current").innerHTML = current.replace(/^\w/, (c) =>
+              c.toUpperCase()
+            );
+            document.getElementById('highTemp').innerHTML=weatherinfo.main.temp_max;
+            document.getElementById('windSpeed').innerHTML=weatherinfo.wind.speed;
+            document.getElementById("humid").innerHTML = weatherinfo.main.humidity;
+          ;
+
+          /*s = wind speed t = temperature*/
+          const s = weatherinfo.wind.speed;
+          const t = weatherinfo.main.temp;
+          let wc =
+            35.74 +
+            0.6215 * t -
+            35.75 * Math.pow(s, 0.16) +
+            0.4275 * t * Math.pow(s, 0.16);
+          wc = Math.round(wc);
+          if (t <= 50 && s > 3) {
+            document.getElementById("chill").textContent = wc + "\xB0" + "F";
+          } else {
+            document.getElementById("chill").textContent = "N/A";
+          }
+
+});
