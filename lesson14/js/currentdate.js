fd1ae2ceb7cd636ca16d45b08810bbfb2e98f90f
taunivance <van12010@byui.edu>
2021-04-02 23:45:22 -0700

    updates

--- lesson14/js/currentdate.js
+++ lesson14/js/currentdate.js
@@ -18,8 +18,8 @@ const months = [
 const todaysdate = new Date();
 const dayName = daynames[todaysdate.getDay()];
 const monthName = months[todaysdate.getMonth()];
+const year = todaysdate.getFullYear();
 const currentdate = dayName + ", " + todaysdate.getDate() + " " + monthName + " " + year;
 const options = {weekday: 'long', month: 'long', day: 'numeric', year: 'numeric'};
-const fulldate = dayName + ", " + monthName + " " + d.getDate() + " " + year;
-document.getElementById("getYear").textContent = year;
+document.getElementById("currentdate").textContent = currentdate;
 
